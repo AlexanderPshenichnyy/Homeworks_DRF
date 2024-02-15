98d3aae44cc55c651778e8ace8e0a8b767994a0d
AlexanderPshenichnyy <ter.anti@mail.ru>
2024-02-15 15:42:00 +0300

    change user

--- users/models.py
+++ users/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+NULLABLE = {
+    'null': True, 'blank': True
+}
+
 
 class User(AbstractUser):
-    first_name = models.CharField(max_length=155,  verbose_name='first_name')
+    first_name = models.CharField(max_length=155, verbose_name='first_name')
     last_name = models.CharField(max_length=155, verbose_name='last_name')
     email = models.EmailField(unique=True, verbose_name='email')
     town = models.CharField(max_length=155, **NULLABLE, verbose_name='town')
